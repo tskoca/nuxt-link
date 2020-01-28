6b9953d0cea8b15b5158870ba98533b452f2be86
Tuğba Sultan KOCA <tubasultankoca@gmail.com>
2020-01-28 09:39:11 +0300

    initial commit

---
+++ pages/products/_productId/edit.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="container">
+            <div class="input-container">
+              <input type="text" v-model="productId">
+              <button class="button--green" @click="changeProductId">Product Id Değiştir</button>
+            </div>
+    </div>
+</template>
+
+<script>
+   export default{
+       data(){
+          return {
+              productId:this.$route.params.productId
+              }
+          
+       },
+       methods:{
+           changeProductId(){
+               this.$router.push('/products/'+this.productId)
+           }
+       }
+ }
+</script>
+
+<style>
+.container {
+    min-height: 100vh;
+    text-align: center;
+    margin-top: 50px;
+  }
+ .input-container{
+    padding: 20px;
+    border: 1px solid #688fb9;
+    margin-top: 50px;
+    background-color: #35495e;
+    box-shadow: 2px 3px 8px rgba(0,0,0,.1);
+
+  }
+
+  input{
+    border: none;
+    outline: none;
+    height: 30px;
+    font-size: 18px;
+    color: #35495e;
+  }
+</style>
---
+++ pages/products/_productId/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container">
+    <h3 class="title">Ürün Özeti</h3>
+    <p class="subtitle"> Ürün ID Bilgisi : {{ $route.params.productId }}</p>
+     <div class="button-container">
+      <a href="/products" class="button--green">Geri</a>
+      <nuxt-link :to="productUrl" tag="a" class="button--green">Düzenle</nuxt-link>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+   computed:{
+       productUrl(){
+         return `/products/${this.$route.params.productId}/edit`
+       }
+   }
+}
+</script>
+<style scoped>
+  .title {
+    font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+    display: block;
+    font-weight: 300;
+    font-size: 100px;
+    color: #35495e;
+    letter-spacing: 1px;
+  }
+
+  .subtitle {
+    margin-top: 10px;
+    font-weight: 300;
+    font-size: 42px;
+    color: #526488;
+    word-spacing: 5px;
+    padding-bottom: 15px;
+  }
+  .container {
+    min-height: 100vh;
+    text-align: center;
+    margin-top: 50px;
+  }
+  .button-container{
+    margin-top: 50px;
+    border: 1px solid #392;
+    background-color: #dedede;
+    padding: 10px;
+    box-shadow: 2px 3px 8px rgba(0,0,0,.1);
+    display: flex;
+    justify-content: center;
+  }
+  .button--green{
+    margin-left: 5px;
+  }
+
+</style>
